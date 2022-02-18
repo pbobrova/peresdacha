6a47fa695522a9b638c6ae7f4f70b1291e581be6
Polina Bobrova <bobrrrovapolina@gmail.com>
2022-02-18 17:11:00 +0300

    "Configure PMD"

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -54,6 +54,7 @@ public abstract class Player {
      * @param player the opponent
      * @param m int corresponding to monster move
      */
+	 @SuppressWarnings("PMD.CognitiveComplexity")
     public void attack(Player player, int m) {
         Random random = new Random();
         double roll = random.nextDouble();
